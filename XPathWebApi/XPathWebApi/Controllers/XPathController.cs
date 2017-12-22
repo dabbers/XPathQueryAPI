38dee7fd3d85d82f3f77cfd3f5a071a7f47598e0
David <dabitp@gmail.com>
2017-12-22 09:56:18 -0800

    MVP, supports select [columns], and nested xpaths.

---
+++ XPathWebApi/XPathWebApi/Controllers/XPathController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using dab.Xpath;
+
+namespace XPathWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class XPathController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Json(QueryProcessor.ProcessQuery(query));
+        }
+        
+    }
+}
---
+++ XPathWebApi/dab.Xpath.Query/QueryProcessor.cs
@@ -0,0 +1,32 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dab.Xpath
+{
+    public static class QueryProcessor
+    {
+        public static dynamic ProcessQuery(string query)
+        {
+            var input = new AntlrInputStream(query);
+
+            htmlapiLexer lexer = new htmlapiLexer(input);
+            lexer.AddErrorListener(new ErrorParserListener());
+
+            CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+            htmlapiParser parser = new htmlapiParser(tokenStream);
+
+            var queryctx = parser.query();
+
+            ParseTreeWalker walker = new ParseTreeWalker();
+            QueryBuilder builder = new QueryBuilder();
+            walker.Walk(builder, queryctx);
+            
+            var result = builder.query.Execute();
+
+            return result;
+        }
+    }
+}
